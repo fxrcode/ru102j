e3e5924d3c7aa3e3026237ad6e33eaec2f84d649
fxrcode <fxrcode@gmail.com>
2021-05-15 23:08:49 -0700

    Week 3: Challenge #5 implement findSitesByGeoWithCapacity

--- src/test/java/com/redislabs/university/RU102J/dao/SiteGeoDaoRedisImplTest.java
+++ src/test/java/com/redislabs/university/RU102J/dao/SiteGeoDaoRedisImplTest.java
@@ -125,7 +125,7 @@ public class SiteGeoDaoRedisImplTest {
     }
 
     // Challenge #5
-    @Ignore
+//    @Ignore
     @Test
     public void findByGeoWithExcessCapacity() {
         SiteGeoDao siteDao = new SiteGeoDaoRedisImpl(jedisPool);
